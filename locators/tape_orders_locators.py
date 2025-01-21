de49933252c02eed4c2d713fafa15f2c9e582759
Andrey Tutunnikov <tt.a.s@yandex.ru>
2025-01-21 20:40:10 +0300

    обновлены локаторы

--- locators/tape_orders_locators.py
+++ locators/tape_orders_locators.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 class TapeOrdersLocators:
 
     ORDERS_TAPE_TITLE = (By.XPATH, '//h1[@class="text text_type_main-large mt-10 mb-5"]')  # Надпись "Лента заказов"
-    FIRST_ORDER = (By.XPATH, '//ul[@class="OrderFeed_list__OLh59"]/li[1]/a') # Первый заказ в ленте заказов
+    FIRST_ORDER = (By.XPATH, '//a[@class="OrderHistory_link__1iNby"]') # Первый заказ в ленте заказов
     COMPOUND_TITLE = (By.XPATH, '//p[@class="text text_type_main-medium mb-8"]')# Надпись "Состав" в модальном окне
     ORDER_ID = (By.XPATH, '//p[@class="text text_type_digits-default"]') # ID заказа
     COUNT_ALL_TIMES = [By.XPATH, '//div[@class="OrderFeed_ordersData__1L6Iv"]/div[2]/p[2]']  # Счетчик  "Выполнено за все время"
