ba48b5a8d5c2d06c4e64321e235b559f8256b3ba
Andrey Tutunnikov <tt.a.s@yandex.ru>
2025-01-21 17:47:11 +0300

    добавлена проверка тестового сценария

--- tests/test_constructor_page.py
+++ tests/test_constructor_page.py
@@ -4,18 +4,18 @@ import data as dt
 
 class TestConstructorPage:
 
-    # @pytest.mark.parametrize('tab, url', [('Конструктор', dt.BASE_URL),
-    #                                       ('Лента Заказов', dt.FEED_URL)])
-    # @allure.title("Тест перехода по кнопкам в хедере")
-    # def test_transition_pages(self, constructor_page, tab, url):
-    #     constructor_page.open_page(dt.LOGIN_URL)
-    #     constructor_page.check_redirect_nav_tab(url, tab)
-    #
-    # @allure.title("Тест работы модального окна с описанием ингредиентов")
-    # def test_ingredient_modal_window(self, constructor_page):
-    #     constructor_page.open_page(dt.BASE_URL)
-    #     constructor_page.check_modal_window_details_displayed()
-    #     constructor_page.check_modal_window_closed()
+    @pytest.mark.parametrize('tab, url', [('Конструктор', dt.BASE_URL),
+                                          ('Лента Заказов', dt.FEED_URL)])
+    @allure.title("Тест перехода по кнопкам в хедере")
+    def test_transition_pages(self, constructor_page, tab, url):
+        constructor_page.open_page(dt.LOGIN_URL)
+        constructor_page.check_redirect_nav_tab(url, tab)
+
+    @allure.title("Тест работы модального окна с описанием ингредиентов")
+    def test_ingredient_modal_window(self, constructor_page):
+        constructor_page.open_page(dt.BASE_URL)
+        constructor_page.check_modal_window_details_displayed()
+        constructor_page.check_modal_window_closed()
 
     @allure.title("Тест создания заказа авторизованным пользователем")
     def test_authorization_user_can_create_order(self, constructor_page, login_page):
